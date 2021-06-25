18108b29b368d3cab403639164415b45a17a7a76
shashwat <shashwat1461.cse18@chitkara.edu.in>
2021-06-25 20:01:53 +0530

    setting up

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+
+const App = () => {
+  return (
+  <div className="grid place-content-center">
+    <h1>
+      hello
+      </h1>
+  </div>
+  );
+};
+
+export default App;
